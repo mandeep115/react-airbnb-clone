f4b352f3fea247560d6e28bfd341ed2f9d79549b
mandeep115 <mandeepab51@gmail.com>
2020-10-11 02:18:18 +0530

    Finished header component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+
+function App() {
+  return (
+    <div className="pp">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
